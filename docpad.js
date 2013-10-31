9c801be78c91b8f0154c1302bfcab5a6a145465c
Renato Vasconcellos <revagomes@gmail.com>
2013-10-31 02:05:20 -0200

    Fixed misspelling.

--- docpad.js
+++ docpad.js
@@ -57,7 +57,7 @@ module.exports = {
       speakers: "Palestrantes",
       schedule: "Programação",
       sponsors: "Patrocínio",
-      partners: "Realiação",
+      partners: "Realiazção",
       contact: "Contato"
     },
 
